8a00dd24a6f26456647a5665cf3254d72a1f794d
LucoMoro <morelli.2000luca@gmail.com>
2025-04-14 14:26:20 +0100

    Add more ".java" file for analysis

---
+++ after/after_CR_1459-1.java
@@ -0,0 +1,25 @@
+Remove unused YouTubeUpload activity
+
+
+
+
+
+
+
+
+
+
+
+
+diff --git a/src/com/android/camera/YouTubeUpload.java b/src/com/android/camera/YouTubeUpload.java
+deleted file mode 100644
+index 0963d09..0000000
+
+@@ -1,145 +0,0 @@
+
+
+
+
+
+
+
